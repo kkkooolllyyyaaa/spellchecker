2f7373ab0906d0cfd4fd92af1a80978fb59c0ab0
swimfish7@niuitmo.ru <kolya_cypandin@mail.ru>
2022-03-26 17:36:16 +0300

    initial

---
+++ src/SpellChecker.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Цыпандин Николай Петрович on 26.03.2022.
+//
+#include <iostream>
+#include "SpellChecker.h"
+
+void SpellChecker::kek() {
+    std::cout << "hello world kek" << std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Цыпандин Николай Петрович on 26.03.2022.
+//
+#include "SpellChecker.h"
+
+int main(int argc, char **argv) {
+    SpellChecker spellChecker;
+    spellChecker.kek();
+    return 0;
+}
